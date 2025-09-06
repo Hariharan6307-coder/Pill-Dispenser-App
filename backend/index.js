1b143a7d23aa06f68633bbfd2170dbdfd50372fc
Hariharan <hariharan.m6307@gmail.com>
2025-09-06 22:38:22 +0530

    setting up the files

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to Supabase
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`✅ Backend running on http://localhost:${PORT}`));
